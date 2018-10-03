d51bc2f6275ef54348b4d63f1b09952f55c5b3e7
DavidWiafe <wiafe.d@hotmail.com>
2018-10-03 16:26:10 -0400

    WEEK 5

---
+++ MVC_MUSIC_STORE/Controllers/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_MUSIC_STORE.Models;
+
+namespace MVC_MUSIC_STORE.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product(String ProductName) {
+
+
+
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+
+        /*public ActionResult Albums() {
+
+            var album = new List<Album>();
+            /*for (int i = 1; i <= 10; i++) {
+                album.add( new Album { Title = "Album" + i.ToString() });
+            }
+
+            return View(ablum);
+        }*/
+
+    }
+}
